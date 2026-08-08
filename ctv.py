d54fcd882388b0580ec7b6d78b71ace5e8ee8f3d
ibrahirahim <ibrahirahimhalil@gmail.com>
2026-08-08 23:54:41 +0300

    Update logo scaling and overlay position

--- ctv.py
+++ ctv.py
@@ -132,8 +132,8 @@ def start_m3u_stream():
             filter_str = (
                 '[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,'
                 'pad=1920:1080:(ow-iw)/2:(oh-ih)/2:black[main];'
-                '[1:v]scale=-2:90[logo];'
-                '[main][logo]overlay=30:30[v]'
+                '[1:v]scale=-2:120[logo];'
+                '[main][logo]overlay=33:33[v]'
             )
             logo_input = ['-i', 'logo.png']
         else:
