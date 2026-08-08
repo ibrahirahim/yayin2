9ead9b98e7c644e5459c41c4fc394e0ddf681563
ibrahirahim <ibrahirahimhalil@gmail.com>
2026-08-08 20:41:52 +0300

    Update logo scaling to 113 pixels in height

--- ctv.py
+++ ctv.py
@@ -131,7 +131,7 @@ def start_m3u_stream():
             filter_str = (
                 '[0:v]scale=1280:720:force_original_aspect_ratio=decrease,'
                 'pad=1280:720:(ow-iw)/2:(oh-ih)/2:black[main];'
-                '[1:v]scale=-2:60[logo];'
+                '[1:v]scale=-2:113[logo];'
                 '[main][logo]overlay=30:30[v]'
             )
             logo_input = ['-i', 'logo.png']
