3b3873f0f75b7cd60109b2e109321ad929d2efe9
ibrahirahim <ibrahirahimhalil@gmail.com>
2026-08-15 16:14:16 +0300

    Update M3U_URL to point to planet.m3u

--- ctv.py
+++ ctv.py
@@ -15,7 +15,7 @@ STREAM_KEY = "planettv"
 RTMP_SERVER = f"{RTMP_URL}/{STREAM_KEY}"
 
 # Yeni M3U ve Logo Bağlantıları
-M3U_URL = "https://raw.githubusercontent.com/ibrahirahim/yayin2/refs/heads/main/action.m3u"
+M3U_URL = "https://raw.githubusercontent.com/ibrahirahim/yayin2/refs/heads/main/planet.m3u"
 LOGO_URL = "https://raw.githubusercontent.com/ibrahirahim/yayin2/refs/heads/main/file_00000000711c8243b56436a5904e0ff5.png"
 
 GIST_ID = "34df90330e4b0daeed9a5b516c1c368d"
